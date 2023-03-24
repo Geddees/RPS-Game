0ebe9122865ebb93d2a19e4f264ce21fe565c830
Nick Mitchell <nickmitchell517@gmail.com>
2023-03-24 01:09:35 -0700

    Add functionality to accept user input and check if it's valid for RPS

--- gamelogic.js
+++ gamelogic.js
@@ -1,9 +1,34 @@
-const rpsChoices = ["Rock", "Paper", "Scissors"]
+const rpsChoices = ["Rock", "Paper", "Scissors"];
+/* let userInput = prompt("Choose your weapon!"); */
 
-
+/* Generates the Computer's Decision from the array rpsChoices */
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * rpsChoices.length)
     return rpsChoices[computerChoice];
 }
 
-console.log(getComputerChoice());
+/* Takes userInput and normalizes it for the game */
+function playerChoice (userInput) {
+    userInput = prompt("Choose your weapon!");
+    let beginningInput = userInput.slice(0,1).toUpperCase();
+    let endingInput = userInput.slice(1).toLowerCase();
+    let pchoice = (beginningInput + endingInput).toString();
+    
+    if (pchoice !== "Rock" && pchoice !== "Paper" && pchoice !== "Scissors"){
+        return alert("Not a valid weapon warrior!");
+    } else {
+        return pchoice;
+    }
+}
+
+/* function validateInput (playerChoice) {
+    console.log(typeof playerChoice); // String
+    if (playerChoice !== "Rock" && playerChoice !== "Paper" && playerChoice !== "Scissors"){
+        return alert("Not a valid weapon warrior!");
+    } else {
+        return console.log(playerChoice);
+    }
+/* }
+/* Check if userInput is a valid choice */
+console.log(getComputerChoice())
+console.log(playerChoice())
