40333639409cee3cbe7b8c8a2295fb4a4a4a86f4
Nick Mitchell <nickmitchell517@gmail.com>
2023-03-31 15:16:04 -0700

    Add functionality that updates W/L tracker

--- gamelogic.js
+++ gamelogic.js
@@ -1,3 +1,4 @@
+const output = document.getElementById("Output");
 const rpsChoices = ["Rock", "Paper", "Scissors"];
 /* let userInput = prompt("Choose your weapon!"); */
 
@@ -6,8 +7,22 @@ function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * rpsChoices.length)
     return rpsChoices[computerChoice];
 }
+
+function updateTracker (){
+    const trackers = document.querySelectorAll(".tracker > div");
+    for (tracker of trackers){
+        if (tracker.getAttribute('class') != 'gamePlayed'){
+            tracker.classList.add("gamePlayed");
+            break
+        } else {
+            continue
+        }
+    }
+    console.log(trackers);
+}
 /* Playing the game */
 function game (){
+    output.textContent =  "";
     /* Getting Player Choice */
     const pChoice = this.getAttribute('id');
     /* Getting Computer Choice */
@@ -15,21 +30,32 @@ function game (){
     console.log("Player: "+ pChoice +" Computer: " + cChoice)
     /* Playing RPS by comparing player and computer choice */
     if (pChoice === cChoice) {
-        return console.log("Draw! Select another weapon now!");      
+        updateTracker();  
+        return output.textContent = "Draw! Select another weapon now!"; 
     } else if ((pChoice === "Rock" && cChoice === "Scissors") 
                 || (pChoice === "Scissors" && cChoice === "Paper") 
                 || (pChoice === "Paper" && cChoice === "Rock" )) {
-        return console.log("You win warrior! A job well done.");
+        updateTracker();
+        return output.textContent = "You win warrior! A job well done.";
     } else {
-        return console.log("You have been defeated... Pick yourself up warrior -- fight another day.");
+        updateTracker();
+        return output.textContent = "You have been defeated... Pick yourself up warrior -- fight another day.";
     }
 }
 /* Add event listeners to the buttons */
 const buttons = document.querySelectorAll('.rpsButton');
-buttons.forEach(button => button.addEventListener("click", game))
+buttons.forEach(button => button.addEventListener("click", game));
 
 
-/* Need to button => get id as player choice => pass choice to function game => 
-    generate computer choice => compare and generate win/loss/tie => output 
-    win/loss to player => mark the w/l tracker
+
+
+/*  How to make a W/L tracker:
+    ???
+    Profit
+
+    Get blank checkbox images
+    add green check on win
+    add red x on loss
+    at 5 games, output the win or loss message
+    make a play again button 
 */
