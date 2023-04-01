3f432478bae571ccfcc0b8485e6ebaddc8c3687b
Nick Mitchell <nickmitchell517@gmail.com>
2023-03-31 17:43:40 -0700

    Add play again button to reset tracker and game scores

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,9 @@
 const output = document.getElementById("Output");
 const rpsChoices = ["Rock", "Paper", "Scissors"];
+const buttons = document.querySelectorAll("#scoreboard > button");
+const trackers = document.querySelectorAll(".tracker > div");
+
+
 /* let userInput = prompt("Choose your weapon!"); */
 
 /* Generates the Computer's Decision from the array rpsChoices */
@@ -8,21 +12,52 @@ function getComputerChoice () {
     return rpsChoices[computerChoice];
 }
 
-function updateTracker (){
-    const trackers = document.querySelectorAll(".tracker > div");
+function updateTracker (className){
     for (tracker of trackers){
-        if (tracker.getAttribute('class') != 'gamePlayed'){
-            tracker.classList.add("gamePlayed");
+        if (tracker.getAttribute('class') === "ready"){
+            tracker.classList.add(className);
             break
         } else {
             continue
         }
     }
-    console.log(trackers);
 }
+/* Win/Loss Tracker */
+let pScore = 0;
+let cScore = 0;
+
+function gamesPlayedTracker (game){
+    (game === "gameWin") ? pScore++ : cScore++;
+    console.log("Player Score: "+pScore)
+    console.log("Computer Score: "+cScore)
+    if (pScore !== 3 && cScore !== 3){
+        return
+    } else if (pScore === 3) {
+        pScore = 0;
+        cScore = 0;
+        output.textContent = "You win warrior! A job well done.";
+        /* Spawns the Play Again Button, and removes the RPS buttons */
+        buttons.forEach(button => button.classList.toggle("hidden"));
+        return 
+    } else {
+        pScore = 0;
+        cScore = 0;
+        output.textContent = "You have been defeated... Pick yourself up warrior -- fight another day."
+        /* Spawns the Play Again Button, and removes the RPS buttons */
+        buttons.forEach(button => button.classList.toggle("hidden"));
+        return
+    }
+}
+
+function resetGame (){
+    output.textContent = ""
+    buttons.forEach(button => button.classList.toggle("hidden"));
+    trackers.forEach(tracker => tracker.classList.remove("gameWin", "gameLoss"))
+}
+
 /* Playing the game */
 function game (){
-    output.textContent =  "";
+    output.textContent = "";
     /* Getting Player Choice */
     const pChoice = this.getAttribute('id');
     /* Getting Computer Choice */
@@ -30,32 +65,35 @@ function game (){
     console.log("Player: "+ pChoice +" Computer: " + cChoice)
     /* Playing RPS by comparing player and computer choice */
     if (pChoice === cChoice) {
-        updateTracker();  
         return output.textContent = "Draw! Select another weapon now!"; 
     } else if ((pChoice === "Rock" && cChoice === "Scissors") 
                 || (pChoice === "Scissors" && cChoice === "Paper") 
                 || (pChoice === "Paper" && cChoice === "Rock" )) {
-        updateTracker();
-        return output.textContent = "You win warrior! A job well done.";
+        updateTracker("gameWin");
+        gamesPlayedTracker("gameWin");
+        return;
     } else {
-        updateTracker();
-        return output.textContent = "You have been defeated... Pick yourself up warrior -- fight another day.";
+        updateTracker("gameLoss");
+        gamesPlayedTracker("gameLoss");
+        return;
     }
 }
 /* Add event listeners to the buttons */
-const buttons = document.querySelectorAll('.rpsButton');
-buttons.forEach(button => button.addEventListener("click", game));
+const rpsButtons = document.querySelectorAll('.rpsButton');
+rpsButtons.forEach(button => button.addEventListener("click", game));
 
+const playAgainButton = document.querySelector(".anotherGame");
+playAgainButton.addEventListener("click", resetGame)
 
-
-
-/*  How to make a W/L tracker:
+function track () {
+    trackers.forEach(tracker => console.log(tracker))
+}
+/*  How to make a play again button:
     ???
-    Profit
-
-    Get blank checkbox images
-    add green check on win
-    add red x on loss
-    at 5 games, output the win or loss message
-    make a play again button 
+    When either player hits 3, spawn a "Play again" button
+    Should remove the RPS buttons if I can...
+    Need to reset: 
+     player and comp scores
+     all the checkboxes
+     remove the win/loss message
 */
